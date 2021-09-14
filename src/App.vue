1e67e125fa86857a7751661377d007d92d312f55
Ryrilya <federicochilese@gmail.com>
2021-09-14 17:45:07 +0200

    Pagination is done

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <h1 class="page-title">Rawfish Pokédex</h1>
+  <header class="main-header">
+    <router-link to="/" class="main-header__link">
+      <h1 class="main-header__brand">Rawfish Pokédex</h1>
+    </router-link>
+  </header>
   <router-view/>
 </template>
 
@@ -15,5 +19,46 @@
 html,
 body {
   font-family: "Josefin Sans", sans-serif;
+}
+
+.main-header {
+  background-color: #2a2a2a;
+  padding: 2rem 5rem;
+
+  &__link {
+    color: white;
+    text-decoration: none;
+  }
+
+  &__brand {
+    font-size: 3em;
+    color: white;
+    cursor: pointer;
+  }
+}
+
+.btn {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: .5rem;
+
+  min-width: 10em;
+  padding: .8rem 1rem;
+  font-weight: bold;
+  font-size: 1em;
+  cursor: pointer;
+  border: none;
+  border-radius: .5rem;
+
+  &--primary {
+    background-color: hsl(19, 85%, 56%);
+    color: white;
+
+    &:disabled {
+      opacity: .5;
+      cursor: not-allowed;
+    }
+  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,68 @@
 <template>
-  <div class="home">
-    <input type="search" placeholder="Bulbasaur" />
-    <button type="button">Search</button>
-    
+  <main class="home">
+    <h2 class="home__page-title">Pokemon List</h2>
+
     <div class="loader" v-if="isLoading">
-      <img class="loader__img" src="@/assets/spinning-mew.gif" alt="loader gif">
+      <img
+        class="loader__img"
+        src="@/assets/spinning-mew.gif"
+        alt="loader gif"
+      />
       <h2 class="loader__subtitle">Catching all Pokemon...</h2>
     </div>
-    <div class="pokemon-cards" v-else>
-      <PokemonCard
-        v-for="pokemon in pokedex"
-        :key="pokemon.id"
-        :pokemon="pokemon"
-      ></PokemonCard>
-    </div>
-  </div>
+
+    <section class="home__main-content" v-show="!isLoading">
+      <div class="home__pagination">
+        <button
+          class="btn btn--primary"
+          type="button"
+          @click="changePage(false)"
+          :disabled="!prevPageUrl"
+        >
+          <fa icon="arrow-left"></fa>
+          Previous
+        </button>
+        <button
+          class="btn btn--primary"
+          type="button"
+          @click="changePage"
+          :disabled="!nextPageUrl"
+        >
+          Next
+          <fa icon="arrow-right"></fa>
+        </button>
+      </div>
+
+      <div class="home__pokemon-cards">
+        <PokemonCard
+          v-for="pokemon in pokedex"
+          :key="pokemon.id"
+          :pokemon="pokemon"
+        ></PokemonCard>
+      </div>
+
+      <div class="home__pagination">
+        <button
+          class="btn btn--primary"
+          type="button"
+          @click="changePage(false)"
+          :disabled="!prevPageUrl"
+        >
+          <fa icon="arrow-left"></fa>
+          Previous
+        </button>
+        <button
+          class="btn btn--primary"
+          type="button"
+          @click="changePage"
+          :disabled="!nextPageUrl"
+        >
+          Next
+          <fa icon="arrow-right"></fa>
+        </button>
+      </div>
+    </section>
+  </main>
 </template>
 
 <script lang="ts">
@@ -33,24 +81,29 @@ export default defineComponent({
   setup() {
     const store = useStore();
     const isLoading = ref<boolean>(false);
-    const nextUrl = ref<string>('');
-    const previousUrl = ref<string>('');
     const pokedex = computed<IPokemon[]>(() => store.state.pokemon || []);
-    const loaderTimeout = 3000;
+    const nextPageUrl = ref<string | null>(null);
+    const prevPageUrl = ref<string | null>(null);
+
+    let loaderTimeout = 3000;
+    let response: IAPIResponse;
 
     onMounted(async () => {
       isLoading.value = true;
 
-      const response = await store.dispatch('getAllPokemon') as IAPIResponse;
-      nextUrl.value = response.next;
-      previousUrl.value = response.previous;
+      response = await store.dispatch('getAllPokemon') as IAPIResponse;
+      nextPageUrl.value = response.next;
+      prevPageUrl.value = response.previous;
 
       loadPokemon(response.results);
-      setTimeout(() => isLoading.value = false, loaderTimeout);
+      setTimeout(() => {
+        isLoading.value = false;
+        loaderTimeout /= 2; // half cuts the fake time needed to load the page
+      }, loaderTimeout);
     });
 
+    /**  Gets pokemon data and save it in store under *pokemon* variable */
     async function loadPokemon(data: ISource[]) {
-
       // Fetch each pokemon data using its url
       const pokemon = await Promise.all(data.map(async _pokemon => await fetchPkmnData(_pokemon.url))) as IPokemon[];
 
@@ -62,41 +115,70 @@ export default defineComponent({
       return await res.json();
     }
 
+    /** Called whenever the user clicks "Next" or "Previous" buttons. It returns the next X pokèmon */
+    async function changePage(next = true) {
+      isLoading.value = true;
+
+      response = await store.dispatch('getAllPokemon', next ? response.next : response.previous) as IAPIResponse;
+      nextPageUrl.value = response.next;
+      prevPageUrl.value = response.previous;
+
+      loadPokemon(response.results);
+      setTimeout(() => isLoading.value = false, loaderTimeout);
+    }
+
     return {
       isLoading,
       pokedex,
-      // goToNextPage,
-      // goToPreviousPage,
+      changePage,
+      nextPageUrl,
+      prevPageUrl,
     };
   }
 });
 </script>
 
 <style lang="scss" scoped>
-.loader {
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+.home {
+  padding: 2rem 5rem;
 
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  &__subtitle {
-    margin-top: 2rem;
-    color: #EFC3D2;
-    text-transform: uppercase;
-    font-family: "Josefin Sans", sans-serif;
+  &__page-title {
+    font-size: 2em;
   }
-}
 
-.pokemon-cards {
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-  column-gap: 1rem;
-  row-gap: 3rem;
-  margin: 5rem;
+  .loader {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    &__subtitle {
+      margin-top: 2rem;
+      color: #efc3d2;
+      text-transform: uppercase;
+      font-family: "Josefin Sans", sans-serif;
+    }
+  }
+
+  &__main-content {
+    .home__pagination {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin: 2rem 0;
+    }
+
+    .home__pokemon-cards {
+      display: grid;
+      grid-template-columns: repeat(5, 1fr);
+      column-gap: 1rem;
+      row-gap: 3rem;
+    }
+  }
 }
 </style>
