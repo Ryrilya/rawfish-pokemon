fe2cbbbcf7f44027e6dd7722f3d952a56dca4df5
Ryrilya <federicochilese@gmail.com>
2021-09-19 19:27:43 +0200

    Added server.js for heroku deployment

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-"use strict";
-exports.__esModule = true;
-var fastify_1 = require("fastify");
-var fastify_static_1 = require("fastify-static");
-var app = fastify_1["default"]();
-app.register(fastify_static_1["default"]);
-var port = 8080;
+const express = require('express');
+const serveStatic = require('serve-static');
+const path = require('path');
+
+const app = express();
+
+app.use('/', express.static(path.join(__dirname, '/dist')));
+
+const port = 8080;
 app.listen(port);
+
+console.log(`Listening on port ${port}`);
--- server.ts
+++
@@ -1,8 +0,0 @@
-import fastify from 'fastify';
-import fastifyStatic from 'fastify-static';
-
-const app = fastify();
-app.register(fastifyStatic);
-
-const port = 8080;
-app.listen(port);
